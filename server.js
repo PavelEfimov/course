c96c504c444b85bdea5d68815da322b467b9cd63
Pavel Efimov <Pavel Efimov>
2017-12-20 22:46:09 +0300

    Создание компонента App.jsx

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, () => console.log('server on port 3000'));
