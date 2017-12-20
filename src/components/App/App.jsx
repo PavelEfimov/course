72a804c007afd3f444147bec96301d1d4e9b5eb5
Pavel Efimov <Pavel Efimov>
2017-12-21 00:58:50 +0300

    Создание ссылок сайта, добавление компонента BrowserRouter в index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import Routes from '../Routes/Routes';
+
 class App extends Component {
     render() {
         return (
-            <div><h1>Hello world</h1></div>
+            <div>
+                <h1>Hello world</h1>
+                {Routes.map((route, index)=>(
+                <Route path={route.path} 
+                    exact={route.exact} 
+                    component={route.component} key={index} />))}
+                <Redirect to="/Home" from="/" />      
+            </div>
         )
     }
 }
