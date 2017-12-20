3c6e997b143678817a36ba8058d46ab4d56dfa5c
Pavel Efimov <Pavel Efimov>
2017-12-21 01:34:36 +0300

    Создание компонента Header, Banner, NavigationMenu, SocialLinks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
 import { render } from 'react-dom';
 
 import Routes from '../Routes/Routes';
---
+++ src/components/App/Header/NavigationMenu.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//import '../../css/Header.css';
+
+const menu = [
+  {link : "/Home", title: " Главная / "},
+  {link : "/Schedule", title: " Расписание богослужений / "},
+  {link : "/Remembrance", title: " Поминовения / "},
+  {link : "/Feedback", title: " Обратная связь / "},
+  {link : "/Contacts", title: " Контакты "},
+]
+
+const NavigationMenu = () => (
+  <div className="navigation-menu">
+    {menu.map((item, index) => (
+      <Link className="menu-link" key={index} to={item.link}>{item.title}</ Link>
+    ))}
+  </div>
+)
+export default NavigationMenu;
