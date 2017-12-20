afe733ff098b0ed55e3b20751186cc04a5941240
Pavel Efimov <Pavel Efimov>
2017-12-21 02:10:25 +0300

    Добавлены некоторые стили для Компонентов Header, App, Home

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,12 @@ module.exports = {
                 test: /\.(js|jsx)?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
-            },  {
+            }, 
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }, 
+            {
                 test: /\.(png|jpg|gif)$/,
                 use: [
                   {
